ac9055ea0d0407431bf18a2158125e5e1262af01
meatbags <jxburrow@gmail.com>
2018-08-23 22:23:09 +1000

    improve eye animation

--- js/modules/ui/nodes/node_view.js
+++ js/modules/ui/nodes/node_view.js
@@ -18,7 +18,7 @@ class NodeView extends NodeBase {
       bez: 6,
       radius: {
         current: 7,
-        min: 6,
+        min: 5,
         max: 7
       }
     };
@@ -27,6 +27,7 @@ class NodeView extends NodeBase {
     this.bez.cp1 = {x: this.eye.width - this.eye.bez, y: 2 + this.eye.bez * 0.75};
     this.bez.cp2 = {x: this.eye.bez, y: this.eye.height};
     this.bez.p2 = {x: 0, y: this.eye.height};
+    this.bez.offset = {current: 0, min: 0, max: 1.5};
     this.distance = -1;
     this.opacity = 1;
   }
@@ -66,22 +67,23 @@ class NodeView extends NodeBase {
 
   draw(ctx) {
     if (this.onscreen && this.active) {
-      this.eye.radius.current += ((this.hover ? this.eye.radius.min : this.eye.radius.max) - this.eye.radius.current) * 0.25;
+      this.eye.radius.current += ((this.hover ? this.eye.radius.max : this.eye.radius.min) - this.eye.radius.current) * 0.25;
+      this.bez.offset.current += ((this.hover ? this.bez.offset.min : this.bez.offset.max) - this.bez.offset.current) * 0.25;
       ctx.globalAlpha = this.opacity;
       ctx.beginPath();
-      ctx.arc(this.coords.x, this.coords.y, this.eye.radius.current, 0, Math.PI * 2);
-      ctx.moveTo(this.coords.x + this.bez.p1.x, this.coords.y + this.bez.p1.y);
-      ctx.bezierCurveTo(this.coords.x + this.bez.cp1.x, this.coords.y + this.bez.cp1.y, this.coords.x + this.bez.cp2.x, this.coords.y + this.bez.cp2.y, this.coords.x + this.bez.p2.x, this.coords.y + this.bez.p2.y);
-      ctx.bezierCurveTo(this.coords.x - this.bez.cp2.x, this.coords.y + this.bez.cp2.y, this.coords.x - this.bez.cp1.x, this.coords.y + this.bez.cp1.y, this.coords.x - this.bez.p1.x, this.coords.y + this.bez.p1.y);
-      ctx.moveTo(this.coords.x - this.bez.p1.x, this.coords.y - this.bez.p1.y);
-      ctx.bezierCurveTo(this.coords.x - this.bez.cp1.x, this.coords.y - this.bez.cp1.y, this.coords.x - this.bez.cp2.x, this.coords.y - this.bez.cp2.y, this.coords.x - this.bez.p2.x, this.coords.y - this.bez.p2.y);
-      ctx.bezierCurveTo(this.coords.x + this.bez.cp2.x, this.coords.y - this.bez.cp2.y, this.coords.x + this.bez.cp1.x, this.coords.y - this.bez.cp1.y, this.coords.x + this.bez.p1.x, this.coords.y - this.bez.p1.y);
+      ctx.arc(this.coords.x, this.coords.y, this.eye.radius.current, Math.PI * 0.5, Math.PI * 2.5);
+      ctx.moveTo(this.coords.x + this.bez.p1.x, this.coords.y + this.bez.p1.y - this.bez.offset.current);
+      ctx.bezierCurveTo(this.coords.x + this.bez.cp1.x, this.coords.y + this.bez.cp1.y - this.bez.offset.current, this.coords.x + this.bez.cp2.x, this.coords.y + this.bez.cp2.y - this.bez.offset.current, this.coords.x + this.bez.p2.x, this.coords.y + this.bez.p2.y - this.bez.offset.current);
+      ctx.bezierCurveTo(this.coords.x - this.bez.cp2.x, this.coords.y + this.bez.cp2.y - this.bez.offset.current, this.coords.x - this.bez.cp1.x, this.coords.y + this.bez.cp1.y - this.bez.offset.current, this.coords.x - this.bez.p1.x, this.coords.y + this.bez.p1.y - this.bez.offset.current);
+      ctx.moveTo(this.coords.x - this.bez.p1.x, this.coords.y - this.bez.p1.y + this.bez.offset.current);
+      ctx.bezierCurveTo(this.coords.x - this.bez.cp1.x, this.coords.y - this.bez.cp1.y + this.bez.offset.current, this.coords.x - this.bez.cp2.x, this.coords.y - this.bez.cp2.y + this.bez.offset.current, this.coords.x - this.bez.p2.x, this.coords.y - this.bez.p2.y + this.bez.offset.current);
+      ctx.bezierCurveTo(this.coords.x + this.bez.cp2.x, this.coords.y - this.bez.cp2.y + this.bez.offset.current, this.coords.x + this.bez.cp1.x, this.coords.y - this.bez.cp1.y + this.bez.offset.current, this.coords.x + this.bez.p1.x, this.coords.y - this.bez.p1.y + this.bez.offset.current);
       ctx.stroke();
 
       if (this.hover) {
-        ctx.beginPath();
-        ctx.arc(this.coords.x, this.coords.y, this.eye.radius.current, 0, Math.PI * 2);
-        ctx.fill();
+        //ctx.beginPath();
+        //ctx.arc(this.coords.x, this.coords.y, this.eye.radius.current, 0, Math.PI * 2);
+        //ctx.fill();
       }
     }
   }
